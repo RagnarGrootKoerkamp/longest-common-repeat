499a6ce637dd6611387b18ab2da4b63d3abd463f
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-12-15 14:57:29 +0100

    Add main.rs wrapper binary

---
+++ ssa/src/main.rs
@@ -0,0 +1,38 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use rand::{seq::SliceRandom, thread_rng};
+use ssa::Ssa;
+
+#[derive(clap::Parser)]
+struct Args {
+    file: PathBuf,
+    density: f32,
+    max_l: usize,
+}
+
+fn main() {
+    let args = Args::parse();
+    // Read std, drop lines starting with >, and concatenate the rest.
+    eprintln!("Reading...");
+    let t = std::fs::read_to_string(args.file)
+        .unwrap()
+        .lines()
+        .skip(1)
+        .collect::<String>();
+    let t = t.as_bytes();
+    eprintln!("Length: {}", t.len());
+    // Read the density as the first command line argument.
+    let density: f32 = args.density;
+    // Take a subset of indices with the given density.
+    let all_idxs = (0..t.len()).collect::<Vec<_>>();
+    let idxs: Vec<_> = all_idxs
+        .choose_multiple(&mut thread_rng(), (density * t.len() as f32) as usize)
+        .cloned()
+        .collect();
+    eprintln!("Idxs: {}", idxs.len());
+    eprintln!("Building...");
+    let start = std::time::Instant::now();
+    let _ssa = Ssa::new(t, idxs, Some(args.max_l));
+    eprintln!("Time: {:?}", start.elapsed());
+}
